fd371d743668065ada0e6dac5dd53219b28cd374
garrettgreen <73969691+garrettgreen@users.noreply.github.com>
2023-04-14 10:45:39 -0700

    Create reverse.asm

---
+++ reverse.asm
@@ -0,0 +1,77 @@
+# Garrett Green & Logan 
+# mod.asm 
+# CPE 315
+
+
+# declare global so programmer can see actual addresses.
+.globl welcome
+.globl prompt
+.globl sumText
+
+#  Data Area (this area contains strings to be displayed during the program)
+.data
+
+welcome:
+	.asciiz " This program returns the reverse of a 32-bit number \n\n"
+
+prompt:
+	.asciiz " Enter an integer: "
+    
+sumText: 
+	.asciiz " \n Reverse = "
+
+#Text Area (i.e. instructions)
+.text
+
+main:
+
+	# Display the welcome message (load 4 into $v0 to display)
+	ori     $v0, $0, 4			
+
+	# This generates the starting address for the welcome message.
+	# (assumes the register first contains 0).
+	lui     $a0, 0x1001
+	syscall
+
+	# Display prompt
+	ori     $v0, $0, 4			
+	
+	# This is the starting address of the prompt (notice the
+	# different address from the welcome message)
+	lui     $a0, 0x1001
+	ori     $a0, $a0,0x22
+	syscall
+
+    # Read 1st integer from the user (numerator)
+	ori     $v0, $0, 5
+	syscall
+
+     # Put num into s0
+    addi $s0, $v0, 0
+
+loop:
+	# $s1 carries counter var
+	addi $s1, $0, 0
+
+	srl $s2, $s0, $s1
+	andi $s3, $s2, 0x01
+	sll $s4, $s4, 1
+	or $s4, $s4, $s3
+	addi $s1, $s1, 1
+
+	blt $s1, 32, loop
+
+	#display text
+    ori     $v0, $0, 4			
+	lui     $a0, 0x1001
+	ori     $a0, $a0,0x36
+	syscall
+
+	# load syscall to print integer and set $a0 to reverse number
+	li $v0, 1
+	addi $a0, $s4, 0
+	syscall
+
+	ori $v0, $0, 10
+    syscall
+
